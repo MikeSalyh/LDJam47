e76441358ec247afd651a18ab68d30e72911e4e3
mikesalyh <mike@salyh.com>
2020-10-03 11:32:43 -0700

    Starting on grid Fx.

---
+++ LoopBack/Assets/Scripts/GUI/WorkerGridPattern.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WorkerGridPattern : MonoBehaviour
+{
+    public float GetCellSize()
+    {
+        return GetComponentInChildren<Image>().GetComponent<RectTransform>().sizeDelta.x;
+    }
+
+    public Vector3[] GetPositions()
+    {
+        Image[] allCells = GetComponentsInChildren<Image>();
+        Vector3[] gridPositions = new Vector3[allCells.Length];
+        for (int i = 0; i < allCells.Length; i++)
+        {
+            gridPositions[i] = allCells[i].GetComponent<RectTransform>().anchoredPosition;
+        }
+        return gridPositions;
+    }
+}
---
+++ LoopBack/Assets/Scripts/GUI/WorkerGridReference.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorkerGridReference : MonoBehaviour
+{
+    private WorkerGridPattern[] positions;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        positions = GetComponentsInChildren<WorkerGridPattern>();    
+    }
+}
