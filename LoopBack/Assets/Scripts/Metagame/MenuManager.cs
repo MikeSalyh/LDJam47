7892b8055163acd32ca65916e6fe69662a7e3f87
mikesalyh <mike@salyh.com>
2020-10-04 13:15:14 -0700

    Instrux.

--- LoopBack/Assets/Scripts/Metagame/MenuManager.cs
+++ LoopBack/Assets/Scripts/Metagame/MenuManager.cs
@@ -1,11 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class MenuManager : MonoBehaviour
 {
+    public GameObject instrux;
+
     public void GoToGameplay()
     {
         MetagameManager.instance.GoToGameplay();
+    }
+
+    public void ShowInstrux()
+    {
+        instrux.gameObject.SetActive(true);
+        instrux.GetComponent<CanvasGroup>().alpha = 0f;
+        instrux.GetComponent<CanvasGroup>().DOFade(1f, 0.5f);
+    }
+
+    public void HideInstrux()
+    {
+        instrux.gameObject.SetActive(false);
     }
 }
